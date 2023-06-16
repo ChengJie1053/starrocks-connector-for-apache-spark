0b995ce37085592c5e3511d4dcbef46db359f233
ChengJie1053 <18033291053@163.com>
2023-06-16 11:06:40 +0800

    修改RestService、AbstractRowStringConverter、StarRocksSink、build.sh

--- src/main/scala/com/starrocks/connector/spark/sql/StarRocksSink.scala
+++ src/main/scala/com/starrocks/connector/spark/sql/StarRocksSink.scala
@@ -1,6 +1,5 @@
 package com.starrocks.connector.spark.sql
 
-import com.alibaba.fastjson2.{JSON, JSONObject}
 import com.starrocks.data.load.stream.StreamLoadManager
 import com.starrocks.data.load.stream.DefaultStreamLoadManager
 import com.starrocks.data.load.stream.properties.StreamLoadProperties
@@ -24,7 +23,7 @@ private[sql] class StarRocksSink private() extends Sink with Serializable {
     this.props = props
     this.sinkManager = new DefaultStreamLoadManager(props)
 
-//    sinkManager.init()
+    //    sinkManager.init()
   }
 
   override def addBatch(batchId: Long, data: DataFrame): Unit = {
